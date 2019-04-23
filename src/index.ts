f3112b03a32a5a34e6a8be8be32ee9353370584e
Adam Fisher <adamnfish@gmail.com>
2019-04-23 16:12:17 +0100

    Add gts dev dependency and take its advice

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import { URL } from 'url';
-
-import { APIGatewayProxyHandler, Context, APIGatewayProxyCallback, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import {APIGatewayProxyCallback, APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult, Context} from 'aws-lambda';
+import {URL} from 'url';
 
 function ok(data: string): APIGatewayProxyResult {
-    return {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        body: data
-    };
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: data
+  };
 }
 
-const handler: APIGatewayProxyHandler = (event: APIGatewayProxyEvent, context: Context, callback: APIGatewayProxyCallback) => {
-    callback(null, ok("ok"));
-};
+const handler: APIGatewayProxyHandler =
+    (event: APIGatewayProxyEvent, context: Context,
+     callback: APIGatewayProxyCallback) => {
+      callback(null, ok('ok'));
+    };
 
-export { handler }
+export {handler};
--- src/local.ts
+++ src/local.ts
@@ -1,38 +1,38 @@
-import express from 'express';
+import {APIGatewayProxyCallback, APIGatewayProxyEvent, Context} from 'aws-lambda';
 import bodyparser from 'body-parser';
+import express from 'express';
+import {Request, Response} from 'express';
 
-import { Request, Response } from 'express';
-import { APIGatewayProxyCallback, APIGatewayProxyEvent, Context } from 'aws-lambda';
-import { handler } from './index';
+import {handler} from './index';
 
 const app = express();
 app.use(bodyparser.json());
 
 function run(path: string, method: string) {
-    return (req: Request, res: Response) => {
-        // This simply has the fields that we end up accessing in the handler
-        const event = {
-            httpMethod: method,
-            path: path.substring('/api'.length),
-            queryStringParameters: req.query,
-            body: req.body ? JSON.stringify(req.body) : undefined
-        }
+  return (req: Request, res: Response) => {
+    // This simply has the fields that we end up accessing in the handler
+    const event = {
+      httpMethod: method,
+      path: path.substring('/api'.length),
+      queryStringParameters: req.query,
+      body: req.body ? JSON.stringify(req.body) : undefined
+    };
 
-        handler(event as APIGatewayProxyEvent, {} as Context, (error, result) => {
-            if(error) {
-                res.status(500).send(error);
-            } else if(result) {
-                const headers = result.headers || {};
+    handler(event as APIGatewayProxyEvent, {} as Context, (error, result) => {
+      if (error) {
+        res.status(500).send(error);
+      } else if (result) {
+        const headers = result.headers || {};
 
-                let resp = res.status(result.statusCode);
-                Object.keys(headers || {}).forEach(header => {
-                    resp.header(header, headers[header] as string);
-                });
-
-                resp.send(result.body);
-            }
+        const resp = res.status(result.statusCode);
+        Object.keys(headers || {}).forEach(header => {
+          resp.header(header, headers[header] as string);
         });
-    }
+
+        resp.send(result.body);
+      }
+    });
+  };
 }
 
 app.get('/api/consent', run('/api/consent', 'GET'));
