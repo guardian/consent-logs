9e96a14b818b8c941c4c50881f45b0e612a9c35c
Adam Fisher <adamnfish@gmail.com>
2019-04-24 15:20:01 +0100

    Add build plumbing

---
+++ teamcity.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+npm install
+# Will place .js files in dist
+npm run build
+
+# These also need to be in the RiffRaff package
+cp package.json dist
+cp riff-raff.yaml dist
+cp cfn.template.yaml dist
+
+pushd dist
+# Ensures the RiffRaff package has the node_modules needed to run
+npm install --production
+popd
+
+npm run package
