9a7d7b498d17fd7d4c182ae447ce92517c161763
Adam Fisher <adamnfish@gmail.com>
2019-04-25 15:52:38 +0100

    Debugging

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
 import {APIGatewayProxyCallback, APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult, Context} from 'aws-lambda';
 import {URL} from 'url';
 
-function ok(data: string): APIGatewayProxyResult {
+function ok(message: string): APIGatewayProxyResult {
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
     },
-    body: data
+    body: JSON.stringify({response: message})
   };
 }
 
 const handler: APIGatewayProxyHandler =
     (event: APIGatewayProxyEvent, context: Context,
      callback: APIGatewayProxyCallback) => {
+      console.log('test');
       callback(null, ok('ok'));
     };
 
